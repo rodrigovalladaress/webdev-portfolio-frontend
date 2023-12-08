7bdc519a8ef802177725c5ed8e04326ada7fad5f
rodrigovalladaress <dim3trodon@gmail.com>
2023-12-08 15:32:06 +0000

    Style nav menu for desktop

--- components/NavMenu.vue
+++ components/NavMenu.vue
@@ -1,9 +1,13 @@
 <template>
-  <nav class="nav full-width flex justify-space-around mono-font lowercase bg-color-trans-90">
+  <nav
+    class="nav full-width width-inherit-lg d-flex justify-space-around align-items-center mono-font lowercase bg-black-a-90 bg-trans-lg"
+  >
     <template v-for="({ label, href }, i) in items" :key="i">
-      <NuxtLink class="flex justify-center align-center" :to="href" active-class="active">
+      <NuxtLink class="d-flex justify-center align-items-center text-medium" :to="href" active-class="active">
         {{ label }}
       </NuxtLink>
+
+      <div v-if="i != items.length - 1" class="d-none d-block-lg pointer-events-none">/</div>
     </template>
   </nav>
 </template>
@@ -22,12 +26,21 @@ const items = [
   left: 0;
   bottom: 0;
   height: 7.4rem;
-
-  // background-color: red;
   font-size: 1.92rem;
+  line-height: 1;
+
+  @include media(lg) {
+    position: inherit;
+    height: fit-content;
+  }
 
   > * {
     flex: 1;
+
+    @include media(lg) {
+      font-size: 3.318rem;
+      margin-top: 0.5rem;
+    }
   }
 
   // Style for very small screens
@@ -47,6 +60,14 @@ const items = [
 .nav {
   .active {
     text-decoration: underline !important;
+
+    @include media(lg) {
+      text-decoration: underline !important;
+      text-decoration-thickness: 2px;
+      text-underline-offset: 10px;
+
+      // border-bottom: 2px $text-color solid;
+    }
   }
 }
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,18 @@
 <template>
-  <div class="layout p-1">
-    <header>
-      <h1>
-        <div class="h1 text-medium">rodrigo valladares santana</div>
+  <header class="d-flex-lg justify-space-between bg-black-a-80-lg p-1">
+    <h1 class="lowercase">
+      <div class="h1 text-medium">
+        Rodrigo <br class="d-none d-block-lg" />
+        Valladares Santana
+      </div>
 
-        <div class="h2">full stack developer</div>
-      </h1>
-    </header>
+      <div class="h2">Full-stack developer</div>
+    </h1>
 
     <NavMenu></NavMenu>
+  </header>
 
+  <div class="layout p-1">
     <slot />
 
     <!-- <footer>Footer</footer> -->
@@ -23,7 +26,7 @@
   line-height: 1.2;
 
   @include media("lg") {
-    line-height: 1;
+    line-height: 1.1;
   }
 }
 
@@ -32,7 +35,7 @@
   margin-top: 1.4rem;
 
   @include media("lg") {
-    margin-top: 2.2rem;
+    margin-top: 2rem;
   }
 }
 
