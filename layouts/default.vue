f86f0d2a5db1830add6fda309bc0221b35ba467a
rodrigovalladaress <dim3trodon@gmail.com>
2023-12-15 10:58:16 +0000

    Fix incorrect padding classes in default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="d-flex-lg justify-space-between">
-    <h1 class="lowercase bg-black-a-80 bg-trans-lg p-1">
+    <h1 class="h1 lowercase bg-black-a-80 bg-trans-lg p-1 p-lg-2">
       <div class="h1 text-medium">
         Rodrigo <br class="d-none d-block-lg" />
         Valladares Santana
@@ -9,10 +9,10 @@
       <div class="h2">Full-stack developer</div>
     </h1>
 
-    <NavMenu class="p-1"></NavMenu>
+    <NavMenu class="p-lg-2"></NavMenu>
   </header>
 
-  <div class="layout p-1">
+  <div class="layout p-1 p-lg-2">
     <slot />
 
     <!-- <footer>Footer</footer> -->
@@ -24,6 +24,9 @@
 <style lang="scss" scoped>
 .h1 {
   line-height: 1.2;
+
+  // Correct the line height top spacing
+  margin-top: -0.2rem;
 
   @include media("lg") {
     line-height: 1.1;
