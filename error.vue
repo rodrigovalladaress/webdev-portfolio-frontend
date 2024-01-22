bf4e722598e6d76243c0a5fe8c7605c94e7f33d1
rodrigovalladaress <dim3trodon@gmail.com>
2024-01-22 12:40:52 +0000

    Show error page

---
+++ error.vue
@@ -0,0 +1,41 @@
+<template>
+  <NuxtLayout>
+    <div v-if="error" class="error-page d-flex column justify-content-center align-items-center p-1 p-lg-2">
+      <h1 v-if="error.statusCode">{{ error.statusCode }}</h1>
+
+      <h1 v-else>Something wrong happened</h1>
+
+      <p class="error-message">
+        <strong v-if="error.message">{{ error.message }}</strong>
+        <strong v-else>{{ error }}</strong>
+      </p>
+
+      <div class="actions">
+        <a class="btn" @click="handleError">Go to home page</a>
+      </div>
+    </div>
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  // eslint-disable-next-line vue/require-default-prop
+  error: Object,
+});
+
+const handleError = () => {
+  clearError();
+
+  window.location.href = "/";
+};
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  font-size: 8rem;
+}
+
+.actions {
+  margin-top: 6rem;
+}
+</style>
