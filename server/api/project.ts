c5cb73422a7d7d798d844957d632301e0b1029b9
rodrigovalladaress <dim3trodon@gmail.com>
2024-01-26 15:03:29 +0000

    Save project description in a different file

--- server/api/project.ts
+++ server/api/project.ts
@@ -3,19 +3,20 @@ import fs from "fs/promises";
 import { Project } from "~/types/project";
 
 const PROJECT_DATA_PATH = "server/data/project";
+const PROJECT_DESCRIPTION_DATA_PATH = "server/data/project-description";
 
 export default defineEventHandler(async (_event) => {
-  let files;
+  let projectFiles;
   try {
-    files = await fs.readdir(PROJECT_DATA_PATH);
+    projectFiles = await fs.readdir(PROJECT_DATA_PATH);
   } catch (err) {
     // eslint-disable-next-line no-console
-    console.error("Error getting files in data/project");
+    console.error(`Error getting files in ${PROJECT_DATA_PATH}`);
     throw err;
   }
 
   const projects = [];
-  for (const fileName of files) {
+  for (const fileName of projectFiles) {
     const filePath = `${PROJECT_DATA_PATH}/${fileName}`;
 
     let projectJson;
@@ -27,16 +28,31 @@ export default defineEventHandler(async (_event) => {
       throw err;
     }
 
-    let project: Project;
+    let parsedProjectJson: Omit<Project, "id"> & { description?: string };
     try {
-      project = JSON.parse(`${projectJson}`);
+      parsedProjectJson = JSON.parse(`${projectJson}`);
     } catch (err) {
       // eslint-disable-next-line no-console
       console.error(`Error parsing JSON at ${filePath}`);
       throw err;
     }
 
-    project.id = fileName.replace(".json", "");
+    const id = fileName.replace(".json", "");
+    const descriptionFileName = `${PROJECT_DESCRIPTION_DATA_PATH}/${id}.html`;
+    let descriptionFromFile = null;
+    try {
+      descriptionFromFile = (await fs.readFile(descriptionFileName)).toString();
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.warn(`Project ${id} doesn't ahve a description HTML file`);
+    }
+
+    const project: Project = {
+      id,
+      ...parsedProjectJson,
+      description: descriptionFromFile ?? parsedProjectJson.description ?? "",
+    };
+    project.id = id;
 
     projects.push(project);
   }
